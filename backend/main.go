e6e0ecdba66d51505a3a34aeaa054f04cdacee63
root <root@192.168.1.27>
2023-02-28 13:21:30 +0100

    added output of active channels and calls as well as calls processed

--- backend/main.go
+++ backend/main.go
@@ -34,6 +34,7 @@ func main() {
 		return
 	}
 
+	// Get number of peers and online/offline status
 	fmt.Fprintf(conn, "Action: Command\r\n")
 	fmt.Fprintf(conn, "Command: sip show peers\r\n")
 	fmt.Fprintf(conn, "\r\n")
@@ -51,9 +52,6 @@ func main() {
 			break
 		}
 	}
-	fmt.Println(response)
-
-	// Parse response to get number of peers and online/offline status
 	var numOnline, numOffline int
 	lines := strings.Split(response, "\n")
 	for _, line := range lines {
@@ -64,7 +62,43 @@ func main() {
 		}
 	}
 
+	// Get channel information
+	fmt.Fprintf(conn, "Action: Command\r\n")
+	fmt.Fprintf(conn, "Command: core show channels\r\n")
+	fmt.Fprintf(conn, "\r\n")
+
+	// Read command response
+	response = ""
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading command response:", err)
+			return
+		}
+		response += string(buf[:n])
+		if n < len(buf) {
+			break
+		}
+	}
+
+	// Count active channels, active calls, and calls processed
+	var numActiveChannels, numActiveCalls, numCallsProcessed int
+	words := strings.Fields(response)
+	for i, word := range words {
+		if word == "active" && words[i+1] == "channels" {
+			fmt.Sscanf(words[i-1], "%d", &numActiveChannels)
+		} else if word == "active" && words[i+1] == "call" {
+			fmt.Sscanf(words[i-1], "%d", &numActiveCalls)
+		} else if word == "calls" && words[i+1] == "processed" {
+			fmt.Sscanf(words[i-1], "%d", &numCallsProcessed)
+		}
+	}
+
+	// Print results
 	fmt.Println("Total users:", numOnline+numOffline)
 	fmt.Println("Online:", numOnline)
 	fmt.Println("Offline:", numOffline)
+	fmt.Printf("Active channels: %d\n", numActiveChannels)
+	fmt.Printf("Active calls: %d\n", numActiveCalls)
+	fmt.Printf("Call processed: %d\n", numCallsProcessed)
 }
